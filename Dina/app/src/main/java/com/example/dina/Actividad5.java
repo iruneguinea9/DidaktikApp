f08f7d26ac74dd79027b792585ddc6b8be66b550
Adrián <adrian.ferrad@gmail.com>
2022-11-28 14:28:29 +0100

    Actividad 5 v1

--- Dina/app/src/main/java/com/example/dina/Actividad5.java
+++ Dina/app/src/main/java/com/example/dina/Actividad5.java
@@ -2,11 +2,25 @@ package com.example.dina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class Actividad5 extends AppCompatActivity {
 
-    private String[] sopa = new String[]{
+    private final HashMap<String, Boolean> palabras = new HashMap<String, Boolean>();
+    private GridView gv;
+    private TextView resul;
+
+
+    private String[] letrasSopa = new String[]{
             "A","S","J","O","P","K","H","O","H","S","E","H",
             "N","R","N","M","C","J","U","I","J","N","D","P",
             "O","C","R","C","T","V","M","V","K","S","M","C",
@@ -20,9 +34,63 @@ public class Actividad5 extends AppCompatActivity {
             "M","A","A","Y","P","J","H","F","N","M","F","C",
             "E","I","T","S","A","S","M","U","S","E","O","T"};
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad5);
+
+        gv = (GridView)findViewById(R.id.gv);
+        resul = findViewById(R.id.tvResultadoSopa);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, R.layout.activity_actividad5_textview, letrasSopa);
+        gv.setAdapter(adaptador);
+
+        final String[] anterior = {""};
+        cargarMapa();
+        gv.setOnTouchListener((view, motionEvent) -> {
+            for(int i = 0; i < gv.getChildCount(); i++) {
+                View v = gv.getChildAt(i);
+                System.out.println("*-*--*-*-*-*-*-*-*-*-*-*");
+                System.out.println(gv.getChildAt(i));
+                System.out.println("*-*--*-*-*-*-*-*-*-*-*-*");
+                Rect outRecto = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                Rect aa = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                if(outRecto.contains((int)motionEvent.getX(), (int)motionEvent.getY())) {
+                    if(!((TextView) v).getText().equals(anterior[0])) {
+                        resul.append(((TextView) v).getText());
+                        anterior[0] = (String) ((TextView) v).getText();
+                        boolean posible = false;
+                        for(String palabra : palabras.keySet()) {
+                            if(!palabras.get(palabra)) {
+                                if (palabra.toUpperCase().startsWith(resul.getText().toString())) {
+                                    posible = true;
+                                }
+                                if (palabra.toUpperCase().equals(resul.getText().toString())) {
+                                    palabras.put(palabra, true);
+                                   // comprobarMapa();
+                                    resul.setText("");
+                                }
+                            }
+                        }
+                        if(!posible) {
+                            resul.setText("");
+                        }
+                    }
+                }
+            }
+            return false;
+        });
     }
+
+    private void cargarMapa() {
+        palabras.put("asjo", false);
+        palabras.put("eitsas", false);
+        palabras.put("arrantzale", false);
+    }
+
+
+
+
+
 }
