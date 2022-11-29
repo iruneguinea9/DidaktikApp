65b6049144bb41307f3e29686f76b3289ab6e489
Deckoner <briankira100@gmail.com>
2022-11-29 09:13:13 +0100

    Esqueleto de Actividad 6

---
+++ Dina/app/src/main/java/com/example/dina/Dialogo_gana_pieza_act6.java
@@ -0,0 +1,14 @@
+package com.example.dina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dialogo_gana_pieza_act6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialogo_gana_pieza_act6);
+    }
+}
