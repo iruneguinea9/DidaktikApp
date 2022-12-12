4e33682e1dfd6ca1bd069e88a7c65e68170186f9
Iruneguinea29 <iruneguinea29@gmail.com>
2022-12-12 08:57:12 +0100

    Marcadores mapa satelite

--- Dina/app/src/main/java/com/example/dina/mapa.java
+++ Dina/app/src/main/java/com/example/dina/mapa.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -69,26 +71,61 @@ public class mapa extends AppCompatActivity  implements OnMapReadyCallback {
     }
     private void insertarMarcador(){
 
-        //  marker = mapa.addMarker(new MarkerOptions().position(new LatLng(40.3936945, -3.701519)) //Madrid
-        //          .title("Pais: España")
-        //          .snippet("La capital de España es Madrid"));
-        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(42.8466, -2.67332)) //Vitoria
-                .title("Pais Vasco: Vitoria")
-                .snippet("Green Capital"));
+        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(43.33450283507816, -3.039349459816046))
+                .title("Sardina saltzaileen omenerako monumentua")
+                );
         marker.setVisible(true);
 
         marker.showInfoWindow();
-        //marker.hideInfoWindow();
+        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(43.333518, -3.038331))
+                .title("Arrantzaleen auzoa")
+        );
+        marker.setVisible(true);
+
+        marker.showInfoWindow();
+        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(43.32900318126162, -3.0323053585039936))
+                .title("Arrantzatzen duen haurraren eskultura")
+        );
+        marker.setVisible(true);
+
+        marker.showInfoWindow();
+        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(43.330333133956486, -3.031112341060796))
+                .title("Sarturtziko portua")
+        );
+        marker.setVisible(true);
+
+        marker.showInfoWindow();
+        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(43.33093367976051, -3.0306794450093046))
+                .title("Arrantzaleen Kofradia edo Arraina saltzeko etxea")
+        );
+        marker.setVisible(true);
+
+        marker.showInfoWindow();
+        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(43.33040796856918, -3.029608358503928))
+                .title("Karmengo Ama Birjina")
+        );
+        marker.setVisible(true);
+
+        marker.showInfoWindow();
+        marker = mapa.addMarker(new MarkerOptions().position(new LatLng(43.33093766293911, -3.0315689315146916))
+                .title("Itsasoko ama: Arraun kluba")
+        );
+        marker.setVisible(true);
+
+        marker.showInfoWindow();
 
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mapa = googleMap;
-        mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
         mapa.getUiSettings().setMapToolbarEnabled(true);
-
+        CameraUpdate camUpd1 =
+                CameraUpdateFactory.newLatLngZoom(new LatLng(43.333149, -3.035268),15);
+        mapa.moveCamera(camUpd1);
+        insertarMarcador();
         mapa.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
