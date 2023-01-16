a077ecfcbc66e94426ab12abb8884af1cd1cd73e
Iruneguinea29 <iruneguinea29@gmail.com>
2023-01-16 08:45:27 +0100

    De creditos al modo desarrollador

--- Dina/app/src/main/java/com/example/dina/Creditos.java
+++ Dina/app/src/main/java/com/example/dina/Creditos.java
@@ -2,13 +2,35 @@ package com.example.dina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Creditos extends AppCompatActivity {
-
+    TextView desarrolladoresTxt;
+    int clickcount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creditos);
+        clickcount = 0;
+        desarrolladoresTxt = findViewById(R.id.garatzaileak);
+
+        desarrolladoresTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickcount += 1;
+                if(clickcount>8){
+                    Toast.makeText(Creditos.this, "Clicks para entrar a modo desarrollador: "+(10-clickcount) , Toast.LENGTH_SHORT).show();
+                    if (clickcount==10){
+                        Intent intent = new Intent(Creditos.this, ParaProbarActividades.class);
+                        startActivity(intent);
+                    }
+                }
+
+            }
+        });
     }
 }
